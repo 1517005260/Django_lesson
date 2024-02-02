cff2ed880698297a5bdbd1e8adcc3302b8a4ea0c
g lk <1517005260@qq.com>
2024-02-02 14:35:24 +0800

    modified web.html

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,8 +1,8 @@
 // 本文件是总文件，用于调用其他文件中的类。由于js在调用之前必须先定义，且我们之前写好了按字典序排序所有js文件的函数，所以zbase会排在后面，即先定义再调用减少报错。
 
 //在html文件中写了：let game = new Game();  就会调用到这里
-class Game{
-    constructor(id){
+export class Game {
+    constructor(id) {
         this.id = id;
         this.$game = $('#' + id);
         this.menu = new GameMenu(this);
@@ -11,7 +11,7 @@ class Game{
         this.start();
     }
 
-    start(){
+    start() {
 
     };
 
